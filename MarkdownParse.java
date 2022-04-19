4aa13fd3f28e2e3e59efa42d514f459265932839
BellaReal <101063556+BellaReal@users.noreply.github.com>
2022-04-18 21:49:52 -0700

    Fix for reading empty lines using Scanner

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -4,21 +4,32 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MarkdownParse {
 
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
+        Scanner scnr = new Scanner(markdown);
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
+        while(scnr.hasNextLine()) { 
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
+
+            // to handle case where one of them loops over the file again
+            if (openBracket < 0 || closeBracket < 0 || openParen < 0 || closeParen < 0) {
+                break;
+            }
+            
             toReturn.add(markdown.substring(openParen + 1, closeParen));
+            
             currentIndex = closeParen + 1;
+            scnr.nextLine();
         }
+        scnr.close();
 
         return toReturn;
     }
